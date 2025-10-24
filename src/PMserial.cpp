c265c3aa2c50db055d06ecd1c821a2d3931f3835
t <t@t>
2025-10-25 06:39:51 +0900

    test#2

--- src/PMserial.cpp
+++ src/PMserial.cpp
@@ -7,7 +7,18 @@
   PMSA003 aka G10
 */
 
+#include "driver/uart.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_timer.h"
+#include "freertos/idf_additions.h"
+#include "freertos/projdefs.h"
+#include "hal/uart_types.h"
+#include "portmacro.h"
 #include <PMserial.h>
+#include <cstddef>
+#include <cstdint>
+#include <iostream>
 
 /* Sensor data format:
   https://github.com/avaldebe/AQmon/blob/master/lua_modules/pms3003.lua
@@ -50,177 +61,268 @@ DATA(MSB,LSB): Message body (28 bytes), 14 pairs of bytes (MSB,LSB)
   14( 31, 32): cksum=byte01+..+byte30
 */
 
-const uint8_t
-    TSI_START = 4,  // PM [ug/m3] (TSI standard)
-    ATM_START = 10, // PM [ug/m3] (std. atmosphere)
-    NUM_START = 16; // num. particles in 100 cm3 of air
+#define UART_BUF_SIZE (128)
+#define RD_BUF_SIZE (40)
 
-const uint8_t
-    msgLen = 7,
-    //act[msgLen] = {0x42,0x4D,0xE1,0x00,0x01,0x01,0x71}, // set active mode
-    slp[msgLen] = {0x42,0x4D,0xE4,0x00,0x00,0x01,0x73}, // sleep
-    wak[msgLen] = {0x42,0x4D,0xE4,0x00,0x01,0x01,0x74}, // wake
-    cfg[msgLen] = {0x42, 0x4D, 0xE1, 0x00, 0x00, 0x01, 0x70}, // set passive mode
-    trg[msgLen] = {0x42, 0x4D, 0xE2, 0x00, 0x00, 0x01, 0x71}; // passive mode read
+static const char TAG[] = "PMSerial";
+static uint8_t *databuf;
+static uint8_t dataLen;
 
-void SerialPM::init()
-{
-#ifdef ESP32
-  if ((hwSerial == serModeHardware) && rx && tx)
-    static_cast<HardwareSerial *>(uart)->begin(9600, SERIAL_8N1, rx, tx);
-#else
+static bool tsi_mode = false;
+static bool truncated_num = false;
 
-#ifdef HAS_HW_SERIAL
-  if (hwSerial == serModeHardware)
-    static_cast<HardwareSerial *>(uart)->begin(9600, SERIAL_8N1);
-#endif
+static QueueHandle_t uart_queue;
+static TaskHandle_t recv_task_handle;
 
-#endif // ESP32
+static bool is_uart_driver_installed = false;
+static bool is_inited = false;
+static bool is_buffered = false;
 
-#ifdef HAS_SW_SERIAL
-  if (hwSerial == serModeSoftware)
-    static_cast<SoftwareSerial *>(uart)->begin(9600);
-#endif
+const uint8_t TSI_START = 4, // PM [ug/m3] (TSI standard)
+    ATM_START = 10,          // PM [ug/m3] (std. atmosphere)
+    NUM_START = 16;          // num. particles in 100 cm3 of air
 
-  uart->write(cfg, msgLen); // set passive mode
-  uart->flush();
-  delay(max_wait_ms * 2);
-  while (uart->available())
-  {
-    uart->read(); // empty the RX buffer
+const uint8_t msgLen = 7,                                          //
+    cmd_actv[msgLen] = {0x42, 0x4D, 0xE1, 0x00, 0x01, 0x01, 0x71}, // set actv
+    cmd_slep[msgLen] = {0x42, 0x4D, 0xE4, 0x00, 0x00, 0x01, 0x73}, // sleep
+    cmd_wkup[msgLen] = {0x42, 0x4D, 0xE4, 0x00, 0x01, 0x01, 0x74}, // wake
+    cmd_pasv[msgLen] = {0x42, 0x4D, 0xE1, 0x00, 0x00, 0x01, 0x70}, // set pasv
+    cmd_read[msgLen] = {0x42, 0x4D, 0xE2, 0x00, 0x00, 0x01, 0x71}; // pasv read
+
+esp_err_t SerialPM::init() {
+  if (uart_is_driver_installed(uart_port)) {
+    ESP_LOGE(
+        TAG,
+        "uart is already initialized, delete driver before call SerialPM.");
+    return ESP_FAIL;
   }
+
+  uart_config_t uart_config;
+  uart_config.baud_rate = 9600;
+  uart_config.data_bits = UART_DATA_8_BITS;
+  uart_config.parity = UART_PARITY_DISABLE;
+  uart_config.stop_bits = UART_STOP_BITS_1;
+  uart_config.flow_ctrl = UART_HW_FLOWCTRL_DISABLE;
+  uart_config.source_clk = UART_SCLK_DEFAULT;
+  uart_config.rx_flow_ctrl_thresh = 122;
+
+  ESP_ERROR_CHECK(uart_driver_install(uart_port, UART_BUF_SIZE * 2,
+                                      UART_BUF_SIZE * 2, 20, &uart_queue, 0));
+  uart_param_config(uart_port, &uart_config);
+  uart_set_pin(uart_port, tx_pin, rx_pin, UART_PIN_NO_CHANGE,
+               UART_PIN_NO_CHANGE);
+  is_uart_driver_installed = true;
+
+  databuf = (uint8_t *)malloc(RD_BUF_SIZE);
+  assert(databuf);
+
+  xTaskCreate(SerialPM::uart_recv_handler_launcher,
+              "uart_recv_handler_launcher", 3072, this, 12, &recv_task_handle);
+
+  ESP_ERROR_CHECK_WITHOUT_ABORT(set_mode(PMSX_SENSOR_MODE_PASSIVE));
+
+  is_inited = true;
+  return ESP_OK;
 }
 
-void SerialPM::sleep() {
-  uart->write(slp, msgLen);  // sleep mode 
-  uart->flush();
-  delay(max_wait_ms * 2);
-}
-void SerialPM::wake() {
-  uart->write(wak, msgLen);  // wake mode 
-  uart->flush();
-  delay(max_wait_ms * 2);
-}
+void SerialPM::uart_recv_handler() {
+  uart_event_t event;
+  while (true) {
+    if (xQueueReceive(uart_queue, (void *)&event, portMAX_DELAY)) {
+      if (event.type == UART_DATA) {
+        memset(databuf, 0, RD_BUF_SIZE);
+        is_buffered = false;
 
-SerialPM::STATUS SerialPM::trigRead()
-{
-  #ifdef HAS_SW_SERIAL
-  if (hwSerial == serModeSoftware)
-    static_cast<SoftwareSerial *>(uart)->listen(); // when you want to listen on a port, explicitly select it. (https://www.arduino.cc/en/Tutorial/LibraryExamples/TwoPortReceive)
-  #endif
+        uart_read_bytes(uart_port, databuf, event.size, portMAX_DELAY);
+        ESP_LOGD(TAG, "data recv");
 
-  while (uart->available())
-  {
-    uart->read(); // empty the RX buffer
+        dataLen = event.size;
+
+        if (!is_inited && dataLen == 1)
+          is_buffered = true;
+
+        // printf("%d: %d: ", dataLen, buff2wd(2));
+        // for (int i = 0; i < dataLen; i += 2)
+        //   printf("%04x ", buff2wd(i));
+        // std::cout << std::endl;
+
+        status = data_checker();
+        if (status == OK) {
+          is_buffered = true;
+          if (dataLen != 8)
+            data_parser(tsi_mode, truncated_num);
+        }
+      }
+    }
   }
-  uart->write(trg, msgLen); // passive mode read
-  uart->flush();
+  std::cout << "handler end\n";
+  return;
+}
 
-  // wait for the mesage header
-  const size_t headLen = 4;     // message header length
-  uint32_t start_ms = millis(); // start waiting time
-  do
-  {                                // ~650ms to complete a measurements
-    delay(10);                     // wait up to max_wait_ms
-    wait_ms = millis() - start_ms; // time waited so far
-  } while (size_t(uart->available()) < headLen && wait_ms < max_wait_ms);
+esp_err_t SerialPM::uart_wait_with_timeout(uint16_t xTimeinMS) {
+  is_buffered = false;
+  int64_t time_wait_until = esp_timer_get_time() + xTimeinMS * 1000;
+  while (!is_buffered) {
+    vTaskDelay(pdMS_TO_TICKS(10));
+    if (esp_timer_get_time() > time_wait_until) {
+      ESP_LOGE(TAG, "UART timed out.");
+      return ESP_FAIL;
+    }
+  }
+  return ESP_OK;
+}
 
-  // we should an answer/message after 650ms
-  if (!uart->available())
-    return ERROR_TIMEOUT;
+esp_err_t SerialPM::uart_send_cmd(uint8_t *cmd) {
+  esp_err_t ret;
+  if (!is_inited) {
+    ret = uart_wait_with_timeout(3000);
+    if (ret != ESP_OK)
+      return ret;
+  }
+  uart_write_bytes(uart_port, cmd, msgLen);
+  ret = uart_wait_tx_done(uart_port, portMAX_DELAY);
+  if (ret != ESP_OK)
+    return ret;
 
-  // read message header
-  nbytes = uart->readBytes(&buffer[0], headLen);
-  if (nbytes != headLen)
+  ret = uart_wait_with_timeout(max_wait_ms);
+  if (ret != ESP_OK)
+    return ret;
+
+  if (cmd != cmd_read && dataLen == 8) {
+    switch (buff2wd(4)) {
+    case 0xe100:
+      ESP_LOGI(TAG, "Mode set to PASV");
+      sensor_mode = PMSX_SENSOR_MODE_PASSIVE;
+      break;
+    case 0xe101:
+      ESP_LOGI(TAG, "Mode set to ACTV");
+      sensor_mode = PMSX_SENSOR_MODE_ACTIVE;
+      break;
+    case 0xe400:
+      ESP_LOGI(TAG, "Device set to SLEEP");
+      sleep_state = PMSX_SENSOR_SLEEP;
+      break;
+    case 0xe401:
+      ESP_LOGI(TAG, "Device set to WakeUP");
+      sleep_state = PMSX_SENSOR_AWAKE;
+      break;
+    default:
+      ESP_LOGW(TAG, "Unknown reply from PMSx device");
+      break;
+    }
+  }
+  return ret;
+}
+
+esp_err_t SerialPM::set_mode(pmsx_sensor_mode_t _mode) {
+  esp_err_t ret;
+  uint8_t *cmd;
+  if (sensor_mode != _mode) {
+    if (_mode == PMSX_SENSOR_MODE_ACTIVE) {
+      cmd = (uint8_t *)cmd_actv;
+    } else {
+      cmd = (uint8_t *)cmd_pasv;
+    }
+    ret = uart_send_cmd(cmd);
+    ESP_LOGI(TAG, "cmd sent");
+    return ret;
+  }
+  return ESP_OK;
+}
+
+esp_err_t SerialPM::set_sleep(pmsx_sensor_sleep_t _sleep) {
+  esp_err_t ret;
+  uint8_t *cmd;
+  if (sleep_state != _sleep) {
+    if (_sleep == PMSX_SENSOR_SLEEP) {
+      cmd = (uint8_t *)cmd_slep;
+    } else {
+      cmd = (uint8_t *)cmd_wkup;
+    }
+    ret = uart_send_cmd(cmd);
+    return ret;
+  }
+  return ESP_OK;
+}
+
+SerialPM::~SerialPM() {
+  vTaskDelete(recv_task_handle);
+  if (is_uart_driver_installed) {
+    ESP_ERROR_CHECK(uart_driver_delete(uart_port));
+    is_uart_driver_installed = false;
+  }
+  free(databuf);
+  databuf = NULL;
+}
+
+uint16_t SerialPM::buff2wd(uint8_t n) {
+  return (databuf[n] << 8) | databuf[n + 1];
+}
+
+pmsx_uart_status_t SerialPM::data_checker() {
+  if (buff2wd(0) != 0x424D)
     return ERROR_MSG_HEADER;
 
-  // message header starts with 'BM'
-  if (buff2word(0) != 0x424D)
-    return ERROR_MSG_START;
-
-  // check message length against stated sensor type
-  size_t bodyLen = buff2word(2);         // message body length
-  size_t messageLen = headLen + bodyLen; // full message length
-  PMS sensor;
-  switch (messageLen)
-  {
-  case 24:
-    sensor = PLANTOWER_24B;
-    break;
-  case 32:
-    sensor = PLANTOWER_32B;
-    break;
-  case 40:
-    sensor = PLANTOWER_40B;
-    break;
-  default:
-    return ERROR_MSG_UNKNOWN;
-  }
-  // self discovery
-  if (pms == PLANTOWER_AUTO)
-    pms = sensor;
-  // check sensor type
-  if (pms != sensor)
-    return ERROR_PMS_TYPE;
-
-  // full message should fit in the buffer
-  if (messageLen > BUFFER_LEN)
+  if (buff2wd(2) + 4 != dataLen)
     return ERROR_MSG_LENGTH;
 
-  // wait for the message body
-  do
-  {                                // ~650ms to complete a measurements
-    delay(10);                     // wait up to max_wait_ms
-    wait_ms = millis() - start_ms; // time waited so far
-  } while (size_t(uart->available()) < bodyLen && wait_ms < max_wait_ms);
+  if (dataLen != 8) { // dataLen 8 is command return
+    pmsx_sensor_type_t sensor_type_detect;
+    switch (dataLen) {
+    case 24:
+      sensor_type_detect = PLANTOWER_24B;
+      break;
+    case 32:
+      sensor_type_detect = PLANTOWER_32B;
+      break;
+    case 40:
+      sensor_type_detect = PLANTOWER_40B;
+      break;
+    default:
+      return ERROR_MSG_UNKNOWN;
+    }
 
-  // we should an answer/message after 650ms
-  if (!uart->available())
-    return ERROR_TIMEOUT;
+    // self discovery
+    if (sensor_type == PLANTOWER_AUTO)
+      sensor_type = sensor_type_detect;
 
-  // read message body
-  nbytes += uart->readBytes(&buffer[headLen], bodyLen);
-  if (nbytes != messageLen)
-    return ERROR_MSG_BODY;
-
-  if (!checkBuffer(messageLen))
+    if (sensor_type != sensor_type_detect) {
+      if (sensor_type_detect == PLANTOWER_32B &&
+          sensor_type != PLANTOWER_32B_S && sensor_type != PLANTOWER_32B_T) {
+        return ERROR_PMS_TYPE;
+      }
+    }
+  }
+  if (!checkBuffer(dataLen))
     return ERROR_MSG_CKSUM;
-
   return OK;
 }
 
-bool SerialPM::checkBuffer(size_t bufferLen)
-{
-  uint16_t cksum = buff2word(bufferLen - 2);
-  for (uint8_t n = 0; n < bufferLen - 2; n++)
-  {
-    cksum -= buffer[n];
+bool SerialPM::checkBuffer(size_t Len) {
+  uint16_t cksum = buff2wd(Len - 2);
+  for (uint8_t n = 0; n < Len - 2; n++) {
+    cksum -= databuf[n];
   }
-
   return (cksum == 0);
 }
 
-void SerialPM::decodeBuffer(bool tsi_mode, bool truncated_num)
-{
+void SerialPM::data_parser(bool tsi_mode, bool truncated_num) {
   uint8_t bin, n;
   if (!has_particulate_matter())
     return;
-  for (bin = 0, n = tsi_mode ? TSI_START : ATM_START; bin < 3; bin++, n += 2)
-  {
-    pm[bin] = buff2word(n);
+  for (bin = 0, n = tsi_mode ? TSI_START : ATM_START; bin < 3; bin++, n += 2) {
+    pm[bin] = buff2wd(n);
   }
 
   if (!has_number_concentration())
     return;
-  for (bin = 0, n = NUM_START; bin < 6; bin++, n += 2)
-  {
-    nc[bin] = buff2word(n); // number particles w/diameter > r_bin
+  for (bin = 0, n = NUM_START; bin < 6; bin++, n += 2) {
+    nc[bin] = buff2wd(n); // number particles w/diameter > r_bin
   }
 
-  switch (pms)
-  {
+  switch (sensor_type) {
   case PMS5003S:
-    hcho = buff2word(28) * 1e-3;
+    hcho = buff2wd(28) * 1e-3;
     break;
   case PMS5003T:
     temp = int16_t(n5p0) * 1e-1 + temp_offset; // cast to signed integer 16bits
@@ -229,9 +331,10 @@ void SerialPM::decodeBuffer(bool tsi_mode, bool truncated_num)
     n10p0 = 0;
     break;
   case PMS5003ST:
-    hcho = buff2word(28) * 1e-3;
-    temp = int16_t(buff2word(30)) * 1e-1 + temp_offset; // cast to signed integer 16bits
-    rhum = buff2word(32) * 1e-1 + rhum_offset;
+    hcho = buff2wd(28) * 1e-3;
+    temp = int16_t(buff2wd(30)) * 1e-1 +
+           temp_offset; // cast to signed integer 16bits
+    rhum = buff2wd(32) * 1e-1 + rhum_offset;
     break;
   default:
     break;
@@ -239,15 +342,13 @@ void SerialPM::decodeBuffer(bool tsi_mode, bool truncated_num)
 
   if (!truncated_num)
     return;
-  for (bin = 0; bin < 5; bin++)
-  {
+  for (bin = 0; bin < 5; bin++) {
     nc[bin] -= nc[bin + 1]; // de-accumulate number concentrations
   }
 }
 
-SerialPM::STATUS SerialPM::read(bool tsi_mode, bool truncated_num)
-{
-  status = trigRead();                   // read comand on passive mode
-  decodeBuffer(tsi_mode, truncated_num); // decode message only if buffer checks out
+pmsx_uart_status_t SerialPM::read(bool tsi_mode, bool truncated_num) {
+  if (sensor_mode == PMSX_SENSOR_MODE_PASSIVE)
+    uart_send_cmd((uint8_t *)&cmd_read);
   return status;
 }
--- src/PMserial.h
+++ src/PMserial.h
@@ -9,35 +9,20 @@
 #ifndef _SERIALPM_H
 #define _SERIALPM_H
 
-#include <Arduino.h>
+#include "esp_err.h"
+#include "hal/uart_types.h"
+#include "soc/gpio_num.h"
+#include <stdint.h>
 
-#define HAS_HW_SERIAL
-#ifdef HAS_HW_SERIAL
-#include <HardwareSerial.h>
-#endif
-
-#if (defined(__AVR__) || defined(ESP8266)) && !defined(NO_SW_SERIAL_REQUIRED)
-#define HAS_SW_SERIAL
-#include <SoftwareSerial.h>
-#endif
-
-#if defined(HAS_HWSERIAL1) || defined(BOARD_HAVE_USART1)
-#define HAS_HW_SERIAL1
-#endif
-
-// leonardo & maple_mini: Serial1 is HWserial
-#if defined(__AVR_ATmega32U4__) || defined(SERIAL_USB)
-#define HAS_USB_SERIAL
-#endif
-
-enum PMS
-{
-  PLANTOWER_AUTO,             // self discovery
-  PLANTOWER_24B,              // 24 byte long message, no count info (LBC)
-  PLANTOWER_32B,              // 32 byte long message, w/count info (LBC)
-  PLANTOWER_32B_S,            // 32 byte long message, w/count info and HCHO (LBC)
-  PLANTOWER_32B_T,            // 32 byte long message, w/partial count info, temp and rhum (LBC)
-  PLANTOWER_40B,              // 40 byte long message, w/count info, temp, rhum and HCHO (LBC)
+typedef enum {
+  PLANTOWER_AUTO,  // self discovery
+  PLANTOWER_24B,   // 24 byte long message, no count info (LBC)
+  PLANTOWER_32B,   // 32 byte long message, w/count info (LBC)
+  PLANTOWER_32B_S, // 32 byte long message, w/count info and HCHO (LBC)
+  PLANTOWER_32B_T, // 32 byte long message, w/partial count info, temp and
+                   // rhum (LBC)
+  PLANTOWER_40B,   // 40 byte long message, w/count info, temp, rhum and HCHO
+                   // (LBC)
   PMSx003 = PLANTOWER_AUTO,   // self discovery
   PMS1003 = PLANTOWER_32B,    // G1
   PMS3003 = PLANTOWER_24B,    // G3
@@ -47,75 +32,29 @@ enum PMS
   PMS5003ST = PLANTOWER_40B,  // G5ST
   PMS7003 = PLANTOWER_32B,    // G7
   PMSA003 = PLANTOWER_32B     // G10
-};
+} pmsx_sensor_type_t;
 
-class SerialPM
-{
-public:
-  union
-  {
-    uint16_t data[9]; // all PM/NC data
-    struct
-    {
-      uint16_t pm[3]; // particulate matter [ug/m3]
-      uint16_t nc[6]; // number concentration [#/100cc]
-    };
-    struct
-    {
-      // pmX [ug/m3]: PM1.0, PM2.5 & PM10
-      uint16_t pm01, pm25, pm10;
-      // nXpY [#/100cc]: number concentrations under X.Y um
-      uint16_t n0p3, n0p5, n1p0, n2p5, n5p0, n10p0;
-    };
-  };
-  union
-  {
-    float extra[3]; // T/RH/HCHO
-    struct
-    {
-      // temperature [°C], relative humidity [%], formaldehyde concentration [mg/m3]
-      float temp, rhum, hcho;
-    };
-  };
+typedef enum {
+  PMSX_SENSOR_MODE_PASSIVE,
+  PMSX_SENSOR_MODE_ACTIVE,
+} pmsx_sensor_mode_t;
 
-  // manual mode - you need configure SerialPort manual and link to this object (function "setSerialPort")
-  SerialPM(PMS sensor) : pms(sensor)
-  {
-    uart = nullptr;
-    hwSerial = serModeManual;
-  }
+typedef enum {
+  PMSX_SENSOR_SLEEP,
+  PMSX_SENSOR_AWAKE,
+} pmsx_sensor_sleep_t;
 
-#ifdef HAS_HW_SERIAL
-  SerialPM(PMS sensor, HardwareSerial &serial) : pms(sensor)
-  {
-    uart = &serial;
-    hwSerial = serModeHardware;
-  }
-#endif
-
-#ifdef HAS_SW_SERIAL
-  SerialPM(PMS sensor, uint8_t rx, uint8_t tx) : pms(sensor)
-  {
-    static SoftwareSerial serial(rx, tx);
-    uart = &serial;
-    hwSerial = serModeSoftware;
-  }
-
-  SerialPM(PMS sensor, Stream &serial) : pms(sensor)
-  {
-    uart = &serial;
-    //TODO: WIP!!!
-    hwSerial = serModeManual;
-  }
-#elif defined(ESP32)
-  SerialPM(PMS sensor, uint8_t rx, uint8_t tx) : pms(sensor), rx(rx), tx(tx)
-  {
-    uart = &Serial1;
-    hwSerial = serModeHardware;
-  }
-#endif
-
-  void init();
+typedef enum {
+  OK,
+  ERROR_TIMEOUT,
+  ERROR_PMS_TYPE,
+  ERROR_MSG_UNKNOWN,
+  ERROR_MSG_HEADER,
+  ERROR_MSG_BODY,
+  ERROR_MSG_START,
+  ERROR_MSG_LENGTH,
+  ERROR_MSG_CKSUM
+} pmsx_uart_status_t;
 
 #define PMS_ERROR_TIMEOUT "Sensor read timeout"
 #define PMS_ERROR_PMS_TYPE "Wrong PMSx003 sensor type"
@@ -126,80 +65,102 @@ public:
 #define PMS_ERROR_MSG_LENGTH "Message too long"
 #define PMS_ERROR_MSG_CKSUM "Wrong message checksum"
 
-  enum STATUS
-  {
-    OK,
-    ERROR_TIMEOUT,
-    ERROR_PMS_TYPE,
-    ERROR_MSG_UNKNOWN,
-    ERROR_MSG_HEADER,
-    ERROR_MSG_BODY,
-    ERROR_MSG_START,
-    ERROR_MSG_LENGTH,
-    ERROR_MSG_CKSUM
+class SerialPM {
+public:
+  union {
+    uint16_t data[9]; // all PM/NC data
+    struct {
+      uint16_t pm[3]; // particulate matter [ug/m3]
+      uint16_t nc[6]; // number concentration [#/100cc]
+    };
+    struct {
+      // pmX [ug/m3]: PM1.0, PM2.5 & PM10
+      uint16_t pm01, pm25, pm10;
+      // nXpY [#/100cc]: number concentrations under X.Y um
+      uint16_t n0p3, n0p5, n1p0, n2p5, n5p0, n10p0;
+    };
+  };
+  union {
+    float extra[3]; // T/RH/HCHO
+    struct {
+      // temperature [°C], relative humidity [%], formaldehyde
+      // concentration [mg/m3]
+      float temp, rhum, hcho;
+    };
   };
 
-  STATUS status;
-  STATUS read(bool tsi_mode = false, bool truncated_num = false);
+  SerialPM(pmsx_sensor_type_t _sensor, uart_port_t _uart_port,
+           gpio_num_t _rx_pin, gpio_num_t _tx_pin)
+      : sensor_type(_sensor), uart_port(_uart_port), rx_pin(_rx_pin),
+        tx_pin(_tx_pin), sensor_mode(PMSX_SENSOR_MODE_ACTIVE),
+        sleep_state(PMSX_SENSOR_AWAKE) {
+    SerialPM::init();
+  };
+
+  // usage SerialPM read();
+  // if sensor is passive, xmit uart pasv read cmd.
+  // if active ,do nothing.
+  pmsx_uart_status_t read(bool tsi_mode = false, bool truncated_num = false);
+  pmsx_uart_status_t get_status() { return status; }
   operator bool() { return status == OK; }
-  void sleep();
-  void wake();
-  inline bool has_particulate_matter() { return status == OK; }
-  inline bool has_number_concentration() { return (status == OK) && (pms != PMS3003); }
-  inline bool has_temperature_humidity() { return (status == OK) && ((pms == PMS5003T) || (pms == PMS5003ST)); }
-  inline bool has_formaldehyde() { return (status == OK) && ((pms == PMS5003S) || (pms == PMS5003ST)); }
-  inline Stream * getSerialPort() { return uart; }
-  inline void setSerialPort(Stream * serial) { uart = serial; hwSerial = serModeManual; }
-    
+
+  esp_err_t set_mode(pmsx_sensor_mode_t _mode);
+  esp_err_t set_sleep(pmsx_sensor_sleep_t _sleep);
+  inline pmsx_sensor_mode_t get_mode() { return sensor_mode; }
+  inline pmsx_sensor_sleep_t get_sleep() { return sleep_state; }
+
   // adding offsets works well in normal range
   // might introduce under- or overflow at the ends of the sensor range
-  inline void  set_rhum_offset(float offset) { rhum_offset = offset; };
-  inline void  set_temp_offset(float offset) { temp_offset = offset; };
+  inline void set_rhum_offset(float _offset) { rhum_offset = _offset; };
+  inline void set_temp_offset(float _offset) { temp_offset = _offset; };
   inline float get_rhum_offset() { return rhum_offset; };
   inline float get_temp_offset() { return temp_offset; };
-#ifdef PMS_DEBUG
-#ifdef HAS_HW_SERIAL
-  inline void print_buffer(Stream &term, const char *fmt)
-  {
-    char tmp[8];
-    for (uint8_t n = 0; n < BUFFER_LEN; n += 2)
-    {
-      sprintf(tmp, fmt, buff2word(n));
-      term.print(tmp);
-    }
-  }
-#endif
-  // debug timing
-  inline uint16_t waited_ms() { return wait_ms; }
-  // debug readBytes
-  inline uint16_t bytes_read() { return nbytes; }
-#endif
+
+  ~SerialPM();
 
 protected:
-  Stream *uart;  // hardware/software serial
-  PMS pms;       // sensor type/message protocol
-  enum {serModeHardware, serModeSoftware, serModeManual} hwSerial; // Is uart hardware serial? (or software serial)
-#ifdef ESP32
-  uint8_t rx, tx; // Serial1 pins on ESP32
-#endif
+  // variables
+  pmsx_sensor_type_t sensor_type; // sensor type/message protocol
+  uart_port_t uart_port;          // uart config
+  gpio_num_t rx_pin, tx_pin;      // uart config
+  pmsx_sensor_mode_t sensor_mode;
+  pmsx_uart_status_t status;
+  pmsx_sensor_sleep_t sleep_state;
 
-  // Correct Temperature & Humidity
-  float temp_offset = 0.0;
+  float temp_offset = 0.0; // Correct Temperature & Humidity
   float rhum_offset = 0.0;
 
+  // Functions
+  esp_err_t init();         // called by constructor
+  void uart_recv_handler(); // uart event handler and wrapper
+  static void uart_recv_handler_launcher(void *args) {
+    static_cast<SerialPM *>(args)->uart_recv_handler();
+    return;
+  }
+
   // utility functions
-  STATUS trigRead();
-  bool checkBuffer(size_t bufferLen);
-  void decodeBuffer(bool tsi_mode, bool truncated_num);
+  uint16_t buff2wd(uint8_t n);
+  bool checkBuffer(size_t Len);
+  esp_err_t uart_send_cmd(uint8_t *cmd);
+  pmsx_uart_status_t data_checker();
+  void data_parser(bool tsi_mode, bool truncated_num);
+  esp_err_t uart_wait_with_timeout(uint16_t xTimeinMS);
 
   // message timing
   static const uint16_t max_wait_ms = 1000;
-  uint16_t wait_ms; // time spent waiting for new sample
 
-  // message buffer
-  static const uint8_t BUFFER_LEN = 40;
-  uint8_t buffer[BUFFER_LEN], nbytes;
-  inline uint16_t buff2word(uint8_t n) { return (buffer[n] << 8) | buffer[n + 1]; }
+  // inline functions
+  inline bool has_particulate_matter() { return status == OK; }
+  inline bool has_number_concentration() {
+    return (status == OK) && (sensor_type != PMS3003);
+  }
+  inline bool has_temperature_humidity() {
+    return (status == OK) &&
+           ((sensor_type == PMS5003T) || (sensor_type == PMS5003ST));
+  }
+  inline bool has_formaldehyde() {
+    return (status == OK) &&
+           ((sensor_type == PMS5003S) || (sensor_type == PMS5003ST));
+  }
 };
-
 #endif //_SERIALPM_H
